a9d051a9fd1fcb811f8dbe85c3905cc819aaa9b7
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2019-07-19 15:35:13 -0400

    Updates

---
+++ precache-manifest.9917c2a3fc3a3cd4a91d53e5c3cd2d4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f3d74e1a1cd51a51e23ecb1ba20dac7",
+    "url": "/woodlanders_react_app/index.html"
+  },
+  {
+    "revision": "f8ec866d2aa94d585f34",
+    "url": "/woodlanders_react_app/static/css/main.6a4f93e0.chunk.css"
+  },
+  {
+    "revision": "44b7d1bcfca409d388fa",
+    "url": "/woodlanders_react_app/static/js/2.c7aca0a8.chunk.js"
+  },
+  {
+    "revision": "f8ec866d2aa94d585f34",
+    "url": "/woodlanders_react_app/static/js/main.bf4c915b.chunk.js"
+  },
+  {
+    "revision": "dca5594fe3cc034b49a1",
+    "url": "/woodlanders_react_app/static/js/runtime~main.59133ef8.js"
+  }
+]);
