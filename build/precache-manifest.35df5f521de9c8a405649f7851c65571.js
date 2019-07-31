18d16d5a3f5b5c68abf1b842917849f5752c2552
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2019-07-30 22:18:49 -0400

    Updates

---
+++ build/precache-manifest.35df5f521de9c8a405649f7851c65571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffc36fc263cc93dcbf6d2aac9a73e78",
+    "url": "/react_prototype/index.html"
+  },
+  {
+    "revision": "7bc9b63b0851cc944a2a",
+    "url": "/react_prototype/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a2c39687aebd40942f9b",
+    "url": "/react_prototype/static/js/2.019922f7.chunk.js"
+  },
+  {
+    "revision": "7bc9b63b0851cc944a2a",
+    "url": "/react_prototype/static/js/main.b4a09e3c.chunk.js"
+  },
+  {
+    "revision": "5d7e2d32f7fb17bac10d",
+    "url": "/react_prototype/static/js/runtime~main.9d0c72cf.js"
+  }
+]);
